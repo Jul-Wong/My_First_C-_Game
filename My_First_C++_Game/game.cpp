f6558435436ad7d564f0212ec394fc2ca9f99fd3
Jul-Wong <julksw@gmail.com>
2024-07-26 17:35:01 +1000

    Added a functional player health bar

--- My_First_C++_Game/game.cpp
+++ My_First_C++_Game/game.cpp
@@ -3,16 +3,24 @@
 #define released(b) !(input->buttons[b].is_down && input->buttons[b].changed)
 
 
-float player_px, player_dpx, player_py, player_dpy;
+float player_px = -20.f, player_dpx, player_py, player_dpy;
 float grass_box_x = 80, grass_box_y = 40;
 float player_half_size_x = 5, player_half_size_y = 5;
 float ball_px, ball_py, ball_dpx = 150, ball_dpy = 30, ball_half_size = 2;
+float health_x;
 
+int static player_health_max = 100, player_health = 100;
 
 internal void simulate_game(Input* input, float dt) {
-    clear_screen(0xe6ff00);
+    clear_screen(0xe6ff00); 
 
     draw_rect(0, 0, grass_box_x, grass_box_y, 0x8dbc49);
+    
+
+    // Player Health Bar
+    draw_rect(0 + 18, grass_box_y + 5, 36, 2, 0xff0000);
+    draw_rect(health_x + 18, grass_box_y + 5, clamp(0, (37 - player_health), 36), 2, 0xffffff);
+    draw_rect(36, grass_box_y + 5, 18, 2, 0xe6ff00);
 
     float player_ddpy = 0.f;
     float player_ddpx = 0.f;
@@ -41,6 +49,8 @@ internal void simulate_game(Input* input, float dt) {
         ball_py + ball_half_size > player_py - player_half_size_y &&
         ball_py - ball_half_size < player_py + player_half_size_y) {
 
+        // Figure a way to interrupt game play loop here to end game when a collision is detected. Or implement a life system?
+        player_health -= 7;
         draw_rect(0, 0, 30, 30, 0xffffff);
     }
     
